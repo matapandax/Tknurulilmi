03ce7b31d01a0ea83c0c3e375d564ec1a87d4f15
matapandax <caesariosatriatama@gmail.com>
2024-04-19 15:20:45 +0700

    first commit

---
+++ application/views/back/layouts/_footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+		<span>Copyright &copy; SMK ASY-SYUHADA 2020 | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+		</span>
+		</div>
+	</div>
+</footer>
